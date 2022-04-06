320b492ae268e5f406c4f847dcdc106acb46670b
AzlanCoding <azlanchenlong@gmail.com>
2022-04-07 06:44:25 +0800

    Update Screen Capture Background Process.py

--- app/Screen Capture Background Process.py
+++ app/Screen Capture Background Process.py
@@ -1,5 +1,7 @@
-import pyautogui
-import os
+import pyscreenshot as ImageGrab
 while True:
-    screenCapture = pyautogui.screenshot()
-    screenCapture.save(r'D:\kjnijijfn.png')
+    # grab fullscreen
+    im = ImageGrab.grab()
+
+    # save image file
+    im.save('D:\kjnijijfn.png')

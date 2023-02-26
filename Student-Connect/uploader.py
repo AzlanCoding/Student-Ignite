db70c26dc5136f8de219ff6f0607b97c8c8dbf14
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2023-02-26 10:35:10 +0800

    Create uploader.py

---
+++ Student-Connect/uploader.py
@@ -0,0 +1,9 @@
+class Uploader:
+  def __init()__:
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+  def Setup():
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    
